7d73902ad5625fb21df1786d859e27a2fce769c0
kristiannahuber@gmail.com <kristiannahuber@gmail.com>
2019-05-22 09:21:56 -0400

    Add project files.

---
+++ Mario/GameScreen.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mario
+{
+    public partial class GameScreen : UserControl
+    {
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //where the game occurs 
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            //create the ground 
+
+            //create the enemies 
+
+            //create the ? boxes 
+
+            //create the player 
+        }
+
+    }
+}
